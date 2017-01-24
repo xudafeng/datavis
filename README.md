411902f46594612e0175d43386211b6c89006bcc
xudafeng <xudafeng@126.com>
2017-01-25 00:16:39 +0800

    add files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # datavis
+
+- http://dc-js.github.io/dc.js/
+- http://gionkunz.github.io/chartist-js/
+- http://c3js.org/samples/chart_step.html
+- https://github.com/c3js/c3
+- http://gor181.github.io/react-chartjs-2/
+- http://www.chartjs.org/docs/#notes-popular-plugins
+- http://echarts.baidu.com/
+- https://antv.alipay.com/g2/doc/index.html
--- javascript/datavis.js
+++ javascript/datavis.js
@@ -4,3 +4,31 @@
 (function(global, undefined) {
 
 })(this);
+
+var chart = dc.barChart("#test");
+//d3.csv("morley.csv", function(error, experiments) {
+var experiments = d3.csv.parse(d3.select('pre#data').text());
+  experiments.forEach(function(x) {
+    x.Speed = +x.Speed;
+  });
+
+  var ndx                 = crossfilter(experiments),
+      runDimension        = ndx.dimension(function(d) {return +d.Run;}),
+      speedSumGroup       = runDimension.group().reduceSum(function(d) {return d.Speed * d.Run / 1000;});
+
+  chart
+    .width(768)
+    .height(480)
+    .x(d3.scale.linear().domain([6,20]))
+    .brushOn(false)
+    .yAxisLabel("This is the Y Axis!")
+    .dimension(runDimension)
+    .group(speedSumGroup)
+    .on('renderlet', function(chart) {
+        chart.selectAll('rect').on("click", function(d) {
+            console.log("click!", d);
+        });
+    });
+    chart.render();
+//});
+
